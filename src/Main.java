4f532cdd67e30708f89b39b32b03c9cf03d399c1
Scott Pantall <aspantall@gmail.com>
2017-05-23 16:31:16 -0600

    Works with happy numbers!

--- src/Main.java
+++ src/Main.java
@@ -17,36 +17,39 @@ public class Main {
         while ((line = buffer.readLine()) != null) {
             line = line.trim();
             System.out.println(happyNumber(line));
-            
-            
         }
     }
     
     public static String happyNumber(String line) {
         String s = "1";
+        
+        if(line.equals(s)) {
+            return s;
+        }
+        
         int result = 0;
         
         char[] digits = line.toCharArray();
         List<Integer> digitList = new ArrayList();
         Function<Integer, Integer> square = x -> x * x;
-            
+
         // Change character array into an ArrayList of integers
         for(char digit : digits) {
             int i = Character.getNumericValue(digit);
             digitList.add(i);
         }
-        
+
         // Square each number in the list`
         digitList = digitList.stream().map(square).collect(toList());
-        
+
         // Add numbers together into a result
         for(int number : digitList) {
             result += number;
         }
-        
+
         // Turn the the result into a string and return it
         s = Integer.toString(result);
 
-        return s;
+        return happyNumber(s);
     }
 }
