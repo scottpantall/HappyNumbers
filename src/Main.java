2af1635bbc4422e6489b928dd3e6594191b15730
Scott Pantall <aspantall@gmail.com>
2017-05-23 16:19:12 -0600

    First iteration of happy number evaluation done

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.function.Function;
+import static java.util.stream.Collectors.toList;
 
 public class Main {
     public static void main (String[] args) throws IOException {
@@ -14,13 +16,37 @@ public class Main {
         
         while ((line = buffer.readLine()) != null) {
             line = line.trim();
-            happyNumber(line);
+            System.out.println(happyNumber(line));
             
             
         }
     }
     
-    public static void happyNumber(String line) {
-        System.out.println(line);
+    public static String happyNumber(String line) {
+        String s = "1";
+        int result = 0;
+        
+        char[] digits = line.toCharArray();
+        List<Integer> digitList = new ArrayList();
+        Function<Integer, Integer> square = x -> x * x;
+            
+        // Change character array into an ArrayList of integers
+        for(char digit : digits) {
+            int i = Character.getNumericValue(digit);
+            digitList.add(i);
+        }
+        
+        // Square each number in the list`
+        digitList = digitList.stream().map(square).collect(toList());
+        
+        // Add numbers together into a result
+        for(int number : digitList) {
+            result += number;
+        }
+        
+        // Turn the the result into a string and return it
+        s = Integer.toString(result);
+
+        return s;
     }
 }
